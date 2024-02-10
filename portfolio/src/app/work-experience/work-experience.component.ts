9b17f6e67907b9ae39168c31d436d4665bf4a92d
Rahul <beingunique2000@gmail.com>
2024-02-10 17:23:41 +0530

    adding work details

--- portfolio/src/app/work-experience/work-experience.component.ts
+++ portfolio/src/app/work-experience/work-experience.component.ts
@@ -12,12 +12,14 @@ export class WorkExperienceComponent {
 
   ngOnInit(){
     let a : workexperiencemodal = {
-      years: 1,
+      years: 1.2,
       role: 'Software Engineer',
       startdate: 'March 2023',
       enddate: 'Present',
       company: 'GE HealthCare',
-      details: ["jhhjhhhkjkjhj","njnjnnnjnj","nbkkbjkjjbjjbbjbbjb"]
+      details: ["Spearheaded the development of a WPF (Windows Presentation Foundation) Application with MVVM Architecture as a Monitoring and Analytics tool covering 80-90% system workflows.",
+      "Equipped the WPF tool with technologies like SQLite for data management and LiveChart for data visualization.",
+      "Assisted the QSE team in enhancing automated testing using MSTest technology. Integrated auto upgrade methods, resulting in an 80% efficiency boost, and reducing the necessity for future template creations and storage."]
     }
     this.workexperience?.push(a);
     console.log(this.workexperience?.length);
