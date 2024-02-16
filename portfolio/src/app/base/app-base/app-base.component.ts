2654c91d660ed02784cc4b80d8b49cf99aad2056
Rahul <beingunique2000@gmail.com>
2024-02-16 16:29:22 +0530

    providing latest update date

--- portfolio/src/app/base/app-base/app-base.component.ts
+++ portfolio/src/app/base/app-base/app-base.component.ts
@@ -1,35 +1,36 @@
 import { Component } from '@angular/core';
 import { Renderer2 } from '@angular/core';
-import { trigger,state,style,animate,transition } from '@angular/animations';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-base',
   templateUrl: './app-base.component.html',
   styleUrls: ['./app-base.component.css'],
-  animations:[
-    trigger('slideInOut',[
-      state('in',style({
-        transform:'translateX(20px)',
-        opacity:1
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translateX(20px)',
+        opacity: 1
       })),
-      transition('void =>*',[
+      transition('void =>*', [
         style({
-          transform:'translateX(90%)',
-          opacity:0
+          transform: 'translateX(90%)',
+          opacity: 0
         }),
         animate('0.3s ease-in-out')
       ]),
-      transition('*=>void',[
-        animate('0.3s ease-in-out',style({
-          transform:'translateZ(100%)',
-          opacity:0
+      transition('*=>void', [
+        animate('0.3s ease-in-out', style({
+          transform: 'translateZ(100%)',
+          opacity: 0
         }))
       ])
     ])
   ]
 })
 export class AppBaseComponent {
-  showAdditionalButton: boolean =false;
+  showAdditionalButton: boolean = false;
+  updateddate: string ="16/02/2024";
   constructor(private renderer: Renderer2) { }
   ToggleTheme() {
     this.showAdditionalButton = !this.showAdditionalButton;
